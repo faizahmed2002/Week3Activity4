39363072ccc7a6430af8fd0cac32ed2928e4a06b
faizahmed2002 <faizahmed2002@gmail.com>
2022-10-04 19:19:09 +0100

    addedjsscript

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,17 @@
 // Write Your JavaScript Code Here
+
+var Name = 'Faiz';
+var Pets =  '2';
+var FunFact = 'Ok'; 
+
+console.log('My name is ' + Name + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun Fact: ' + funFact);
+
+Name = Ahmed;
+Pets = 1;
+FunFact = 'I eat food'; 
+
+console.log('My name is ' + Name + '.');
+console.log('I have ' + Pets + ' pet(s).');
+console.log('Fun fact: ' + FunFact);
